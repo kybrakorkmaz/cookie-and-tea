f00bf17dee53244c50f1f1de044bb53e9a824624
unknown <kybra.korkmaz@gmail.com>
2026-04-17 22:35:22 +0300

    coderabbit suggestions -2 todo:fix navling props

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import {BrowserRouter, Route, Routes} from "react-router";
 
 const App = ()=>{
   return(
-      <BrowserRouter>
-          <Routes>
-              <Route path={"/"} element={<Home/>}/>
-          </Routes>
-      </BrowserRouter>
+      <Routes>
+          <Route path={"/"} element={<Home/>}/>
+          {/*
+          <Route path={"/faq"} element={<Faq/>}/>
+          <Route path={"/your-passions"} element={<YourPassions/>}/>
+          <Route path={"/login"} element={<Login/>}/>
+          <Route path={"/sign-up"} element={<SignUp/>}/>
+          <Route path={"*"} element={<NotFound/>}/>*/}
+      </Routes>
 
   )
 }
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,10 @@
+import {Link} from "react-router";
+
 const Logo = () => {
     return (
-        <a href="/" id="logo" className="flex items-center font-logo">
+        <Link to="/" id="logo" className="flex items-center font-logo">
             C<img id="logo-img" src="/images/logo.png" alt="logo" />T
-        </a>
+        </Link>
     );
 };
 
